322dadca3a32b0d35ccc9bdc109c3dbb7d040016
Matt Holloway <mholloway@book.com>
2014-02-23 15:42:22 -0800

    Removed ActiveRecord

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
+# require "active_resource/railtie"
+require "rails/rack"
 
 Bundler.require(*Rails.groups)
 require "wungine"
