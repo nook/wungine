1c3507736df4fb5e2633e49e18f0d1e12fd76fa9
Matt Holloway <mholloway@book.com>
2014-02-25 17:33:22 -0800

    Added weather controller

---
+++ app/controllers/wungine/weather_controller.rb
@@ -0,0 +1,11 @@
+module Wungine
+  class WeatherController < Wungine::ApplicationController
+    def index
+
+    end
+
+    def show
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Wungine::Engine.routes.draw do
+  root to: 'weather#index'
 end
